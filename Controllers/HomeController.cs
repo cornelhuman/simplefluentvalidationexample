3cfdd112f87396778b6b6c1311e67e515a2cbc05
Cornel <cornelhuman@gmail.com>
2021-09-01 20:08:55 +0100

    Example implemented

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValidationFilterTest.Models;
+
+namespace ValidationFilterTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            return Task.FromResult<IActionResult>(Ok("Hello World"));
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Post([FromBody] CreateContactRequest contactRequest)
+        {
+            return Task.FromResult<IActionResult>(Ok("Hello World"));
+        }
+    }
+}
---
+++ Validator/CreateContactRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValidationFilterTest.Models;
+
+namespace ValidationFilterTest.Validator
+{
+    public class CreateContactRequestValidator : AbstractValidator<CreateContactRequest>
+    {
+        public CreateContactRequestValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Name).NotNull();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Id).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Id).LessThan(10);
+        }
+    }
+}
